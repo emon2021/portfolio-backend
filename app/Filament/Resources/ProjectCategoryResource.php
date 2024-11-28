237f783059ad0671f994d4e5581ad880e03d8003
emon2021 <mdemonahmed2021@gmail.com>
2024-11-29 00:09:30 +0600

    project category added

---
+++ app/Filament/Resources/ProjectCategoryResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Faker\Provider\ar_EG\Text;
+use App\Models\ProjectCategory;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProjectCategoryResource\Pages;
+use App\Filament\Resources\ProjectCategoryResource\RelationManagers;
+
+class ProjectCategoryResource extends Resource
+{
+    protected static ?string $model = ProjectCategory::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->placeholder('Category Name')
+                    ->required()
+                    ->minLength(3)
+                    ->maxLength(255)
+                    ->reactive()
+                    ->live(true)
+                    ->afterStateUpdated(fn (callable $set, $state) => $set('slug', Str::slug($state))), // Automatically sets slug
+
+                TextInput::make('slug')
+                    ->placeholder('Auto-generated based on name')
+                    ->readOnly()
+                    ->required()
+                    ->minLength(3)
+                    ->maxLength(255),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('slug'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    // Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProjectCategories::route('/'),
+            'create' => Pages\CreateProjectCategory::route('/create'),
+            'edit' => Pages\EditProjectCategory::route('/{record}/edit'),
+        ];
+    }
+}
